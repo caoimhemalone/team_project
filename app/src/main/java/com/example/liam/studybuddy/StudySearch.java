22e018d6d273085e73c06237c3e11d757ad288c8
Liame1996 <liam.english1996@gmail.com>
2016-10-11 10:20:58 +0100

    First

---
+++ app/src/main/java/com/example/liam/studybuddy/StudySearch.java
@@ -0,0 +1,13 @@
+package com.example.liam.studybuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudySearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_search);
+    }
+}
