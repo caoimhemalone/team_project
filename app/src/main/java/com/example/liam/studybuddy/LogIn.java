6252ca3a0d8ce6b7d84193964ea1b86cfa544b24
Caoimhe Malone <caoimhegmalone@gmail.com>
2016-10-16 19:38:43 +0100

    Put in GUI for Timetable & study search. Subject to change. Added 2 buttons in login page just so that I could see those two pages as we havent put the menu in yet

--- app/src/main/java/com/example/liam/studybuddy/LogIn.java
+++ app/src/main/java/com/example/liam/studybuddy/LogIn.java
@@ -1,7 +1,11 @@
 package com.example.liam.studybuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    //ONLY TEMPORARY WHILE CAOIMHE SORTS OUT GUI SO SHE CAN SEE THE SPINNER
+    public void openSS (View view){
+        Intent intent = new Intent(this, StudySearch.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //ONLY TEMPORARY WHILE CAOIMHE SORTS OUT GUI SO SHE CAN SEE THE TIMETABLE
+    public void openTT (View view){
+        Intent intent = new Intent(this, Timetable.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/liam/studybuddy/StudySearch.java
+++ app/src/main/java/com/example/liam/studybuddy/StudySearch.java
@@ -1,13 +1,38 @@
+//GOOD SPINNER EXAMPLE HTTPS://WWW.MKYONG.COM/ANDROID/ANDROID-SPINNER-DROP-DOWN-LIST-EXAMPLE/
+
 package com.example.liam.studybuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class StudySearch extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_search);
+
+       // this.arraySpinner = new String[]{
+               // "School of Computing", "Course", "Year", "Module"
+
+
+        //Spinner for school of computing
+        Spinner spin_comp = (Spinner) findViewById(R.id.ss_spinner_comp);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.comp_ss_spinner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spin_comp.setAdapter(adapter);
+
+        // Spinner for school of business
+        Spinner spin_bus  = (Spinner) findViewById(R.id.ss_spinner_bus);
+
+        ArrayAdapter<CharSequence> adapter_2 = ArrayAdapter.createFromResource(this, R.array.bus_ss_spinner_array, android.R.layout.simple_spinner_item);
+        adapter_2.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        spin_bus.setAdapter(adapter_2);
     }
+
 }
