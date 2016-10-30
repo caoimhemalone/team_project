50ef18abe113afed3e196172571ee66f9ff49bba
Liame1996 <liam.english1996@gmail.com>
2016-10-30 17:47:06 +0000

    Database

--- app/src/main/java/com/example/liam/studybuddy/DBHelper.java
+++ app/src/main/java/com/example/liam/studybuddy/DBHelper.java
@@ -1,9 +1,64 @@
 package com.example.liam.studybuddy;
 
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
+ * @reference https://www.youtube.com/user/BowToKingBen
  * Created by Evan on 17/10/2016.
  */
 
 public class DBHelper {
+    private Connection conn;
+    private String TAG = DBHelper.class.getSimpleName();
 
+    private DBHelper(){
+        conn = null;
+        try {
+            Class.forName(AppConfig.DRIVER);
+            conn = DriverManager.getConnection(AppConfig.connectionString, AppConfig.db_user, AppConfig.db_pass);
+            createTable();
+        } catch (SQLException s) {
+            Log.e(TAG, s.getMessage());
+        } catch (ClassNotFoundException c) {
+            Log.e(TAG, c.getMessage());
+        }
+    }
+
+    public void createTable() {
+        String query = "CREATE TABLE IF NOT EXISTS " + AppConfig.TABLE_NAME + "(id int PRIMARY KEY auto_increment, " + "fName varchar(50)not null, " + "lName varchar(50)not null, " + "email varchar(50) not null, " + "repeatEmail varchar(50) not null, " + "password varchar(50) not null, " + "repeatPassword varchar(50) not null, );";
+        try {
+            Statement st = conn.createStatement();
+            st.executeUpdate(query);
+        } catch (SQLException s) {
+            Log.e(TAG, s.getMessage());
+        }
+    }
+
+    public boolean addDetails(String fName, String lName, String email, String repeatEmail, int password, int repeatPassword) {
+        boolean result = false;
+
+        try {
+            PreparedStatement st = conn.prepareStatement("INSERT INTO " + AppConfig.TABLE_NAME + " (fName, lNAme, email, repeatEmail, password, reapeatPassword) VALUES(?,?,?,?,?,?)");
+            st.setString(1, fName);
+            st.setString(2, lName);
+            st.setString(3, email);
+            st.setString(4, repeatEmail);
+            st.setInt(5, password);
+            st.setInt(5, repeatPassword);
+
+            result = st.execute();
+            st.close();
+        } catch (SQLException s) {
+            Log.e(TAG, s.getMessage());
+        }
+        return result;
+    }
 }
+
+
