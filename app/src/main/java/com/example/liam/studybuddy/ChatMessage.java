7c996994b3232fb3e3f4cf13373692503aa5acf1
EvanByrne <evanbyrne89@gmail.com>
2016-11-07 12:44:15 +0000

    Calendar and message update

---
+++ app/src/main/java/com/example/liam/studybuddy/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.example.liam.studybuddy;
+
+/**
+ * Created by Evan on 05/11/2016.
+ */
+
+public class ChatMessage {
+
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message){
+
+        super();
+        this.left=left;
+        this.message=message;
+    }
+}
