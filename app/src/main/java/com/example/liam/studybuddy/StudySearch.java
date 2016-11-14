9abc12c330299cd1cbb74dd4ebcdf1ec1c02fa4c
Liame1996 <liam.english1996@gmail.com>
2016-11-14 14:58:19 +0000

    All NavDrawer

--- app/src/main/java/com/example/liam/studybuddy/StudySearch.java
+++ app/src/main/java/com/example/liam/studybuddy/StudySearch.java
@@ -3,8 +3,15 @@
 package com.example.liam.studybuddy;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import android.view.View;
@@ -19,7 +26,7 @@ import android.widget.ExpandableListView.OnGroupExpandListener;
 
 
 
-public class StudySearch extends AppCompatActivity {
+public class StudySearch extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
@@ -32,6 +39,17 @@ public class StudySearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         backBTN = (ImageButton)findViewById(R.id.backBTN);
         backBTN.setOnClickListener(new View.OnClickListener(){
@@ -127,6 +145,51 @@ public class StudySearch extends AppCompatActivity {
         listDataChild.put(listDataHeader.get(0), school_bus); //Header, Child data
         listDataChild.put(listDataHeader.get(1), school_comp);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
